95e01b72f89c08718a127bf9c12c6d24413ff8ca
mishellgrove <70043762+mishellgrove@users.noreply.github.com>
2021-03-29 00:30:19 -0500

    Update ClientAccount.java

--- src/model/ClientAccount.java
+++ src/model/ClientAccount.java
@@ -1,5 +1,66 @@
 package model;
 
-public class ClientAccount {
+public class ClientAccount implements Comparable<ClientAccount> {
+
+	private String names;
+	private String surnames;
+	private String id;
+	private String address;
+	private String phone;
+	private long timeToFind;
+
+	public ClientAccount (String names, String surnames, String id, String address,String phone) {
+
+		this.names = names;
+		this.surnames = surnames;
+		this.id = id;
+		this.address = address;
+		this.phone = phone;
+	}
+	
+	@Override
+	public int compareTo(ClientAccount c) {
+		int comparator;		
+		comparator = surnames.compareTo(c.getSurnames());
+		if (comparator == 0) {
+			comparator =names.compareTo(c.getName()); 
+		}		
+		return  comparator;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public String getName() {
+		return names;
+	}
+	public String getSurnames() {
+		return surnames;
+	}
+
+	public void setId(String id2) {
+		id = id2;
+	}
+
+	public void setPhone(String phon) {
+		phone = phon;
+	}
+
+	public void setAddress(String add) {
+		address = add;
+	}
+	public long getTimeToFind() {
+		return timeToFind;
+	}
+	public void setTimeToFind(long time) {
+		timeToFind = time;
+	}
 
 }
